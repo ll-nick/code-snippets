8c94d87bca0700a90a5fb458d7c7fb98e7e8bd78
Nick Le Large <nick.lelarge@kit.edu>
2021-04-23 11:58:41 +0200

    Added ensure_dir.py

---
+++ python/ensure_dir.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import os
+
+"""
+Script to check if a directory exists that creates it in case it doesn't.
+"""
+
+def ensure_dir(d):
+    if not os.path.isdir(d):
+        Path(d).mkdir(parents=True)  
+
+def example():
+    # Define example transformation
+    path = "/tmp/test/test2"
+
+    ensure_dir(path)
+
+if __name__=="__main__":
+    example()
